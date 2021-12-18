9e009dcf667e514eb1a4272b941a8e1a6a6b2f31
mrjackmaster <jacektrg+github@gmail.com>
2021-12-18 01:17:05 +0100

    Schema for item

---
+++ models/item.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+	name: { type: String, minlength: 1, maxlength: 50, required: true },
+	description: { type: String, minlength: 1, maxlength: 50, required: true },
+	category: {
+		type: Schema.Types.ObjectId,
+		ref: 'Category',
+		// required: true, // Have to comment out for now, getting ValidationError: category: Path `category` is required. Looking for solution.
+	},
+	price: { type: Number, min: 0, required: true },
+	stock: { type: Number, min: 0, required: true },
+});
+
+ItemSchema.virtual('availability').get(() => {
+	let availability = `In stock:` + this.stock;
+	if (!this.stock) {
+		availability = `Out of stock`;
+	}
+	return availability;
+});
+
+ItemSchema.virtual('url').get(() => {
+	return '/' + this._id;
+});
+
+module.exports = mongoose.model('Item', ItemSchema);
