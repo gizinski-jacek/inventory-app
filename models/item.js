f5ed8cc0e4c96129d8fcade34c5bd5c6e7132b9b
mrjackmaster <jacektrg+github@gmail.com>
2021-12-20 22:32:49 +0100

    Minor change to name and description length

--- models/item.js
+++ models/item.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
-	name: { type: String, minlength: 1, maxlength: 50, required: true },
-	description: { type: String, minlength: 1, maxlength: 50, required: true },
+	name: { type: String, minlength: 1, maxlength: 20, required: true },
+	description: { type: String, minlength: 1, maxlength: 100 },
 	category: {
 		type: Schema.Types.ObjectId,
 		ref: 'Category',
