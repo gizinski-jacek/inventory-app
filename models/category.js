9addcb40aa7d35a35358d8754ef911ad57a525b6
mrjackmaster <jacektrg+github@gmail.com>
2021-12-20 22:33:10 +0100

    Minor change to name and description length.

--- models/category.js
+++ models/category.js
@@ -3,12 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
-	name: { type: String, minlength: 1, maxlength: 50, required: true },
-	description: {
-		type: String,
-		minlength: 10,
-		maxlength: 100,
-	},
+	name: { type: String, minlength: 1, maxlength: 20, required: true },
+	description: { type: String, minlength: 1, maxlength: 100 },
 });
 
 CategorySchema.virtual('url').get(function () {
