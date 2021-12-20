ebf4067b8b6e359e31fea5bc68ac00faa2ff089c
mrjackmaster <jacektrg+github@gmail.com>
2021-12-20 22:39:20 +0100

    Controller for item create GET and POST routes.

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -14,3 +14,81 @@ exports.item_details = (req, res, next) => {
 		});
 	});
 };
+
+exports.item_create_get = (req, res, next) => {
+	Category.find().exec((err, category_list) => {
+		if (err) {
+			return next(err);
+		}
+		res.render('item_form', {
+			title: 'Create item',
+			category_list: category_list,
+		});
+	});
+};
+
+exports.item_create_post = [
+	body('name', 'Item name must not be empty')
+		.trim()
+		.isLength({ min: 1, max: 20 })
+		.escape(),
+	body('description').trim().isLength({ max: 100 }).escape(),
+	body('category').escape(),
+	body('price', 'Item price must not be empty')
+		.trim()
+		.isNumeric({ min: 0 })
+		.escape(),
+	body('stock', 'Item stock must not be empty')
+		.trim()
+		.isNumeric({ min: 0 })
+		.escape(),
+	(req, res, next) => {
+		const errors = validationResult(req);
+		const item = new Item({
+			name: req.body.name,
+			description: req.body.description
+				? req.body.description
+				: req.body.name,
+			category: req.body.category,
+			price: req.body.price,
+			stock: req.body.stock,
+		});
+		if (!errors.isEmpty()) {
+			Category.find().exec((err, category_list) => {
+				if (err) {
+					return next(err);
+				}
+				res.render('item_form', {
+					title: 'Create item',
+					category_list: category_list,
+					item: item,
+					errors: errors.array(),
+				});
+			});
+			return;
+		} else {
+			item.save((err) => {
+				if (err) {
+					return next(err);
+				}
+				res.redirect(`/catalog/${req.body.category}${item.url}`);
+			});
+		}
+	},
+];
+
+exports.item_delete_get = (req, res, next) => {
+	res.render('item_delete', { title: 'Delete category' });
+};
+
+exports.item_delete_post = (req, res, next) => {
+	res.render('item_delete', { title: 'Delete category' });
+};
+
+exports.item_update_get = (req, res, next) => {
+	res.render('item_form', { title: 'Delete category' });
+};
+
+exports.item_update_post = (req, res, next) => {
+	res.render('item_form', { title: 'Delete category' });
+};
