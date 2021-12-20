2a897e148b03a0a339aa7dbdd08e08ab59cce082
mrjackmaster <jacektrg+github@gmail.com>
2021-12-20 22:34:06 +0100

    Adding routes for creating items and categories, and for deleting and updating categories.

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-	res.send('Catalog page');
-	// res.render('index', { title: 'Home' });
-});
+const catalog_controller = require('../controllers/catalogController');
+const category_controller = require('../controllers/categoryController');
+const item_controller = require('../controllers/itemController');
+
+router.get('/', catalog_controller.catalog_index);
+
+router.get('/item/create', item_controller.item_create_get);
+
+router.post('/item/create', item_controller.item_create_post);
+
+router.get('/category/create', category_controller.category_create_get);
+
+router.post('/category/create', category_controller.category_create_post);
+
+router.get('/category/:id/delete', category_controller.category_delete_get);
+
+router.post('/category/:id/delete', category_controller.category_delete_post);
+
+router.get('/category/:id/update', category_controller.category_update_get);
+
+router.post('/category/:id/update', category_controller.category_update_post);
 
 module.exports = router;
