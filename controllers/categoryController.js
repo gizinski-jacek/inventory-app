f319083f2a53dfe3aa30b90d1b5a1766aa5f86f4
mrjackmaster <jacektrg+github@gmail.com>
2021-12-21 18:25:54 +0100

    Preventing creating duplicate categories. Controller for category delete GET and POST routes.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -32,6 +32,7 @@ exports.category_index = (req, res, next) => {
 			res.render('item_list', {
 				title: results.category.name,
 				item_list: results.item_list,
+				category: results.category,
 			});
 		}
 	);
@@ -48,36 +49,102 @@ exports.category_create_post = [
 		.escape(),
 	body('description').trim().isLength({ max: 100 }).escape(),
 	(req, res, next) => {
-		const errors = validationResult(req);
-		const category = new Category({
-			name: req.body.name,
-			description: req.body.description
-				? req.body.description
-				: req.body.name,
-		});
-		if (!errors.isEmpty()) {
-			res.render('category_form', {
-				title: 'Create category',
-				category: req.body,
-				errors: errors.array(),
-			});
-		} else {
-			category.save((err) => {
+		Category.find({ name: req.body.name })
+			.countDocuments()
+			.exec((err, count) => {
 				if (err) {
 					return next(err);
 				}
-				res.redirect(category.url);
+				if (count > 0) {
+					res.render('category_form', {
+						title: 'Create category',
+						category: req.body,
+						errors: [{ msg: 'Category already exists' }],
+					});
+				} else {
+					const errors = validationResult(req);
+					const category = new Category({
+						name: req.body.name,
+						description: req.body.description
+							? req.body.description
+							: req.body.name,
+					});
+					if (!errors.isEmpty()) {
+						res.render('category_form', {
+							title: 'Create category',
+							category: req.body,
+							errors: errors.array(),
+						});
+					} else {
+						category.save((err) => {
+							if (err) {
+								return next(err);
+							}
+							res.redirect(category.url);
+						});
+					}
+				}
 			});
-		}
 	},
 ];
 
 exports.category_delete_get = (req, res, next) => {
-	res.render('category_delete', { title: 'Delete category' });
+	async.parallel(
+		{
+			category: (cb) => {
+				Category.findById(req.params.id).exec(cb);
+			},
+			category_items: (cb) => {
+				Items.find({ category: req.params.id }).exec(cb);
+			},
+		},
+		(err, results) => {
+			if (err) {
+				return next(err);
+			}
+			if (results.category == null) {
+				res.redirect('/catalog');
+			}
+			res.render('category_delete', {
+				title: 'Delete category',
+				category: results.category,
+				category_items: results.category_items,
+			});
+		}
+	);
 };
 
 exports.category_delete_post = (req, res, next) => {
-	res.render('category_delete', { title: 'Delete category' });
+	async.parallel(
+		{
+			category: (cb) => {
+				Category.findById(req.params.id).exec(cb);
+			},
+			category_items: (cb) => {
+				Items.find({ category: req.params.id }).exec(cb);
+			},
+		},
+		(err, results) => {
+			if (err) {
+				return next(err);
+			}
+			if (results.category_items.length > 0) {
+				res.render('category_delete', {
+					title: 'Delete category',
+					category: results.category,
+					results: results.category_items,
+				});
+				return;
+			} else {
+				Category.findByIdAndDelete(req.params.id, (err) => {
+					if (err) {
+						return next(err);
+					}
+					res.redirect('/catalog');
+				});
+			}
+		}
+	);
 };
 
 exports.category_update_get = (req, res, next) => {
