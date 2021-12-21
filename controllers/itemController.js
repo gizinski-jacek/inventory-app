36f57f1b25cb16776e014bf6a13321efe85faf62
mrjackmaster <jacektrg+github@gmail.com>
2021-12-21 23:28:41 +0100

    Controllers for item delete and update GET and POST routes.

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -78,17 +78,96 @@ exports.item_create_post = [
 ];
 
 exports.item_delete_get = (req, res, next) => {
-	res.render('item_delete', { title: 'Delete category' });
+	Item.findById(req.params.id).exec((err, item) => {
+		if (err) {
+			return next(err);
+		}
+		if (item == null) {
+			res.redirect(req.baseUrl);
+		}
+		res.render('item_delete', { title: 'Delete item', item: item });
+	});
 };
 
 exports.item_delete_post = (req, res, next) => {
-	res.render('item_delete', { title: 'Delete category' });
+	Item.findByIdAndDelete(req.params.id).exec((err, item) => {
+		if (err) {
+			return next(err);
+		}
+		res.redirect(req.baseUrl);
+	});
 };
 
 exports.item_update_get = (req, res, next) => {
-	res.render('item_form', { title: 'Delete category' });
+	async.parallel(
+		{
+			item: (cb) => {
+				Item.findById(req.params.id).exec(cb);
+			},
+			category_list: (cb) => {
+				Category.find().exec(cb);
+			},
+		},
+		(err, results) => {
+			if (err) {
+				return next(err);
+			}
+			res.render('item_form', {
+				title: 'Update category',
+				item: results.item,
+				category_list: results.category_list,
+			});
+		}
+	);
 };
 
-exports.item_update_post = (req, res, next) => {
-	res.render('item_form', { title: 'Delete category' });
-};
+exports.item_update_post = [
+	body('name', 'Item name must not be empty')
+		.trim()
+		.isLength({ min: 1, max: 30 })
+		.escape(),
+	body('description').trim().isLength({ max: 100 }).escape(),
+	body('category').escape(),
+	body('price', 'Item price must not be empty')
+		.trim()
+		.isNumeric({ min: 0 })
+		.escape(),
+	body('stock', 'Item stock must not be empty')
+		.trim()
+		.isNumeric({ min: 0 })
+		.escape(),
+	(req, res, next) => {
+		const errors = validationResult(req);
+		const item = new Item({
+			name: req.body.name,
+			description: req.body.description
+				? req.body.description
+				: req.body.name,
+			category: req.body.category,
+			price: req.body.price,
+			stock: req.body.stock,
+			_id: req.params.id,
+		});
+		if (!errors.isEmpty()) {
+			Category.find().exec((err, category_list) => {
+				if (err) {
+					return next(err);
+				}
+				res.render('item_form', {
+					title: 'Create item',
+					category_list: category_list,
+					item: item,
+					errors: errors.array(),
+				});
+			});
+			return;
+		} else {
+			Item.findByIdAndUpdate(req.params.id, item, (err, theitem) => {
+				if (err) {
+					return next(err);
+				}
+				res.redirect(`/catalog/${req.body.category}${theitem.url}`);
+			});
+		}
+	},
+];
