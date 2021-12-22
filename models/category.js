8f7cfe574fd216a87a625bf17e3733c3c2f4089a
mrjackmaster <jacektrg+github@gmail.com>
2021-12-22 18:45:40 +0100

    Reducing category max name length

--- models/category.js
+++ models/category.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
-	name: { type: String, minlength: 1, maxlength: 20, required: true },
+	name: { type: String, minlength: 1, maxlength: 15, required: true },
 	description: { type: String, minlength: 1, maxlength: 100 },
 	permanent: { type: Boolean, default: 0 },
 });
