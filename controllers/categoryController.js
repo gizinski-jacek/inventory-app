c39363922db120a606a639e5d2c8748a10aee50f
mrjackmaster <jacektrg+github@gmail.com>
2021-12-25 21:52:18 +0100

    Implementing admin password and temporary AdminSchema for deleting and updating. Checking if item is permanent and if provided password matches against any available admin passwords in db.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,6 @@
 const Category = require('../models/category');
 const Item = require('../models/item');
+const Admin = require('../models/admin');
 const async = require('async');
 const { body, validationResult } = require('express-validator');
 
@@ -43,7 +44,7 @@ exports.category_create_get = (req, res, next) => {
 exports.category_create_post = [
 	body('name', 'Category name must not be empty')
 		.trim()
-		.isLength({ min: 1, max: 15 })
+		.isLength({ min: 3, max: 15 })
 		.escape(),
 	body('description').trim().isLength({ max: 100 }).escape(),
 	(req, res, next) => {
@@ -70,7 +71,7 @@ exports.category_create_post = [
 					if (!errors.isEmpty()) {
 						res.render('category_form', {
 							title: 'Create category',
-							category: req.body,
+							category: category,
 							errors: errors.array(),
 						});
 					} else {
@@ -112,38 +113,71 @@ exports.category_delete_get = (req, res, next) => {
 	);
 };
 
-exports.category_delete_post = (req, res, next) => {
-	async.parallel(
-		{
-			category: (cb) => {
-				Category.findById(req.params.id).exec(cb);
+exports.category_delete_post = [
+	body('adminpass', 'Must provide admin password for this action')
+		.if(body('categoryispermanent').equals('true'))
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	(req, res, next) => {
+		async.parallel(
+			{
+				category: (cb) => {
+					Category.findById(req.params.id).exec(cb);
+				},
+				category_items: (cb) => {
+					Item.find({ category: req.params.id }).exec(cb);
+				},
+				adminpass: (cb) => {
+					Admin.find({ adminpass: req.body.adminpass }).exec(cb);
+				},
 			},
-			category_items: (cb) => {
-				Item.find({ category: req.params.id }).exec(cb);
-			},
-		},
-		(err, results) => {
-			if (err) {
-				return next(err);
-			}
-			if (results.category_items.length > 0) {
-				res.render('category_delete', {
-					title: 'Delete category',
-					category: results.category,
-					results: results.category_items,
-				});
-				return;
-			} else {
-				Category.findByIdAndDelete(req.params.id, (err) => {
-					if (err) {
-						return next(err);
+			(err, results) => {
+				if (err) {
+					return next(err);
+				}
+				if (results.category_items.length > 0) {
+					res.render('category_delete', {
+						title: 'Delete category',
+						category: results.category,
+						category_items: results.category_items,
+					});
+					return;
+				}
+				if (results.category.permanent) {
+					if (
+						req.body.categoryispermanent.toString() ===
+						results.category.permanent.toString()
+					) {
+						if (results.adminpass.length === 0) {
+							res.render('category_delete', {
+								title: 'Delete category',
+								category: results.category,
+								category_items: results.category_items,
+								pass_check: false,
+							});
+							return;
+						} else {
+							Category.findByIdAndDelete(req.params.id, (err) => {
+								if (err) {
+									return next(err);
+								}
+								res.redirect('/catalog');
+							});
+						}
 					}
-					res.redirect('/catalog');
-				});
+				} else {
+					Category.findByIdAndDelete(req.params.id, (err) => {
+						if (err) {
+							return next(err);
+						}
+						res.redirect('/catalog');
+					});
+				}
 			}
-		}
-	);
-};
+		);
+	},
+];
 
 exports.category_update_get = (req, res, next) => {
 	Category.findById(req.params.id).exec((err, category) => {
@@ -160,35 +194,83 @@ exports.category_update_get = (req, res, next) => {
 exports.category_update_post = [
 	body('name', 'Category name must not be empty')
 		.trim()
-		.isLength({ min: 1, max: 15 })
+		.isLength({ min: 3, max: 15 })
 		.escape(),
 	body('description').trim().isLength({ max: 100 }).escape(),
+	body('adminpass', 'Must provide admin password for this action')
+		.if(body('categoryispermanent').equals('true'))
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
 	(req, res, next) => {
-		const errors = validationResult(req);
-		const category = new Category({
-			name: req.body.name,
-			description: req.body.description
-				? req.body.description
-				: req.body.name,
-			_id: req.params.id,
-		});
-		if (!errors.isEmpty()) {
-			res.render('category_form', {
-				title: 'Update category',
-				category: req.body,
-				errors: errors.array(),
-			});
-		} else {
-			Category.findByIdAndUpdate(
-				req.params.id,
-				category,
-				(err, thecategory) => {
-					if (err) {
-						return next(err);
-					}
-					res.redirect(thecategory.url);
+		async.parallel(
+			{
+				category: (cb) => {
+					Category.findById(req.params.id).exec(cb);
+				},
+				adminpass: (cb) => {
+					Admin.find({ adminpass: req.body.adminpass }).exec(cb);
+				},
+			},
+			(err, results) => {
+				if (err) {
+					return next(err);
 				}
-			);
-		}
+				const errors = validationResult(req);
+				const category = new Category({
+					name: req.body.name,
+					description: req.body.description
+						? req.body.description
+						: req.body.name,
+					permanent: results.category.permanent,
+					_id: req.params.id,
+				});
+				if (!errors.isEmpty()) {
+					res.render('category_form', {
+						title: 'Update category',
+						category: category,
+						errors: errors.array(),
+					});
+					return;
+				}
+				if (results.category.permanent) {
+					if (
+						req.body.categoryispermanent.toString() ===
+						results.category.permanent.toString()
+					) {
+						if (results.adminpass.length === 0) {
+							res.render('category_form', {
+								title: 'Update category',
+								category: category,
+								pass_check: false,
+							});
+							return;
+						} else {
+							Category.findByIdAndUpdate(
+								req.params.id,
+								category,
+								(err, thecategory) => {
+									if (err) {
+										return next(err);
+									}
+									res.redirect(thecategory.url);
+								}
+							);
+						}
+					}
+				} else {
+					Category.findByIdAndUpdate(
+						req.params.id,
+						category,
+						(err, thecategory) => {
+							if (err) {
+								return next(err);
+							}
+							res.redirect(thecategory.url);
+						}
+					);
+				}
+			}
+		);
 	},
 ];
