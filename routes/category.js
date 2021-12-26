de4f3198292e05b790e69a721cf5668a3f64eedf
mrjackmaster <jacektrg+github@gmail.com>
2021-12-26 20:56:58 +0100

    Cleanup, update, fix.

--- routes/category.js
+++ routes/category.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 
-const category_controller = require('../controllers/categoryController');
 const item_controller = require('../controllers/itemController');
 
-router.get('/', category_controller.category_index);
+router.get('/', item_controller.category_item_list);
 
 router.get('/item/:id/delete', item_controller.item_delete_get);
 
@@ -17,7 +16,7 @@ router.post('/item/:id/update', item_controller.item_update_post);
 router.get('/item/:id', item_controller.item_details);
 
 router.get('/item', (req, res, next) => {
-	res.redirect('/');
+	res.redirect('./');
 });
 
 module.exports = router;
