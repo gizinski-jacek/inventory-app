7ab4eed7a8b4e5d63e2fa2cfccb3ddb08db7dfd8
mrjackmaster <jacektrg+github@gmail.com>
2021-12-27 21:29:07 +0100

    Refactoring and updating with new routes structure. Ignoring create routes, not displaying directory bar.

--- controllers/navbarController.js
+++ controllers/navbarController.js
@@ -4,55 +4,66 @@ const async = require('async');
 
 exports.navbar_data = (req, res, next) => {
 	const pathItems = req.path.slice(1).split('/');
-	async.parallel(
-		{
-			category_list: (cb) => {
-				Category.find().exec(cb);
-			},
-			item: (cb) => {
-				Item.findById(pathItems[3]).exec(cb);
-			},
-		},
-		(err, results) => {
+	if (
+		pathItems[2] == 'update' ||
+		pathItems[2] == 'delete' ||
+		req.path == '/catalog/category/create' ||
+		req.path == '/catalog/item/create'
+	) {
+		Category.find().exec((err, category_list) => {
 			if (err) {
 				return next(err);
 			}
-			let modifiedPath = pathItems.filter((item) => item !== '');
-			if (pathItems[1]) {
-				modifiedPath[1] = results.category_list.find((cat) => {
-					return cat._id == pathItems[1];
-				});
-			}
-			if (results.item) {
-				modifiedPath[3] = results.item;
-			}
-			if (modifiedPath.length) {
-				let temp;
-				modifiedPath = modifiedPath.map((ele, i) => {
-					if (typeof ele === 'object') {
-						if (!ele.category) {
-							temp = ele;
-							return { name: ele.name, setPath: ele.url };
-						} else {
-							return {
-								name: ele.name,
-								setPath: temp.url + ele.url,
-							};
-						}
-					}
-					if (i === 0) {
-						return { name: ele, setPath: '/' + ele };
-					} else if (i === 2) {
-						return { name: ele, setPath: '../' };
-					} else {
-						return { name: ele, setPath: './' };
-					}
-				});
-			}
-			res.locals.category_list = results.category_list;
-			res.locals.current_directory = modifiedPath;
-			console.log(res.locals);
+			res.locals.nav_category_list = category_list;
+			res.locals.nav_current_directory = [];
 			next();
-		}
-	);
+		});
+	} else {
+		async.parallel(
+			{
+				category_list: (cb) => {
+					Category.find().exec(cb);
+				},
+				item: (cb) => {
+					Item.findById(pathItems[2]).exec(cb);
+				},
+			},
+			(err, results) => {
+				if (err) {
+					return next(err);
+				}
+				let modifiedPath = pathItems.filter((item) => item !== '');
+				if (modifiedPath.length) {
+					let temp;
+					modifiedPath = modifiedPath.map((ele, i) => {
+						if (
+							ele == 'catalog' ||
+							ele == 'category' ||
+							ele == 'create' ||
+							ele == 'item'
+						) {
+							return { name: ele, setPath: `/${ele}` };
+						} else if (ele == 'delete' || ele == 'update') {
+							return { name: ele, setPath: `./${ele}` };
+						} else {
+							if (modifiedPath.indexOf(ele) === 1) {
+								temp = results.category_list.find((categ) => {
+									return categ._id == ele;
+								});
+								return { name: temp.name, setPath: temp.url };
+							} else if (modifiedPath.indexOf(ele) === 2) {
+								return {
+									name: results.item.name,
+									setPath: `${temp.url}${results.item.url}`,
+								};
+							}
+						}
+					});
+				}
+				res.locals.nav_category_list = results.category_list;
+				res.locals.nav_current_directory = modifiedPath;
+				next();
+			}
+		);
+	}
 };
