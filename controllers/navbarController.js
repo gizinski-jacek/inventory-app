db9efc53493259f81cbcef40b002cf10a047fbab
mrjackmaster <jacektrg+github@gmail.com>
2021-12-28 23:21:23 +0100

    Fixing, updating routes, cleanup.

--- controllers/navbarController.js
+++ controllers/navbarController.js
@@ -1,24 +1,22 @@
 const Category = require('../models/category');
 const Item = require('../models/item');
 const async = require('async');
+const mongoose = require('mongoose');
 
 exports.navbar_data = (req, res, next) => {
 	const pathItems = req.path
 		.slice(1)
 		.split('/')
 		.filter((item) => item !== '');
-	let modifiedPath = pathItems;
 	if (
-		pathItems[2] == 'update' ||
-		pathItems[2] == 'delete' ||
-		req.path == '/catalog/category/create' ||
-		req.path == '/catalog/item/create'
+		req.path == '/category/create' ||
+		req.path == '/item/create' ||
+		req.path == '/'
 	) {
 		Category.find().exec((err, category_list) => {
 			if (err) {
 				return next(err);
 			}
-
 			res.locals.nav_category_list = category_list;
 			res.locals.nav_current_directory = [];
 			next();
@@ -30,41 +28,46 @@ exports.navbar_data = (req, res, next) => {
 					Category.find().exec(cb);
 				},
 				item: (cb) => {
-					Item.findById(pathItems[2]).exec(cb);
+					if (mongoose.Types.ObjectId.isValid(pathItems[2])) {
+						Item.findById(pathItems[2]).exec(cb);
+					} else {
+						cb();
+					}
 				},
 			},
 			(err, results) => {
 				if (err) {
 					return next(err);
 				}
-				if (modifiedPath.length) {
-					let temp;
-					modifiedPath = modifiedPath.map((ele, i) => {
-						if (
-							ele == 'catalog' ||
-							ele == 'category' ||
-							ele == 'create' ||
-							ele == 'item'
-						) {
+				let modifiedPath;
+				if (pathItems.length) {
+					let categ;
+					modifiedPath = pathItems.map((ele) => {
+						if (ele == 'catalog') {
 							return { name: ele, setPath: `/${ele}` };
 						} else if (ele == 'delete' || ele == 'update') {
-							return { name: ele, setPath: `./${ele}` };
+							return { name: ele, setPath: `` };
 						} else {
-							if (modifiedPath.indexOf(ele) === 1) {
-								temp = results.category_list.find((categ) => {
-									return categ._id == ele;
+							if (pathItems.indexOf(ele) === 1) {
+								categ = results.category_list.find((cat) => {
+									return cat._id == ele;
 								});
-								return { name: temp.name, setPath: temp.url };
-							} else if (modifiedPath.indexOf(ele) === 2) {
+								return {
+									name: categ.name,
+									setPath: categ.url,
+								};
+							} else if (
+								pathItems.indexOf(ele) === 2 &&
+								results.item
+							) {
 								return {
 									name: results.item.name,
-									setPath: `${temp.url}${results.item.url}`,
+									setPath: `${categ.url}${results.item.url}`,
 								};
 							}
 						}
 					});
 				}
-				console.log(modifiedPath);
 				res.locals.nav_category_list = results.category_list;
 				res.locals.nav_current_directory = modifiedPath;
 				next();
