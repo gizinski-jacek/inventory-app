84e1edc7b73835e8aa5c502a49c3706b0c240379
mrjackmaster <jacektrg+github@gmail.com>
2022-01-01 01:43:20 +0100

    Cleanup while preparing for deploy.

--- models/admin.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const Schema = mongoose.Schema;
-
-const AdminSchema = new Schema({
-	adminpass: '',
-});
-
-module.exports = mongoose.model('Admin', AdminSchema);
